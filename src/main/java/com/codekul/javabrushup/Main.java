71e6e399d070b79e12583a9e45d29a60b8d90c16
aniruddha <aniruddha.kudalkar@gmail.com>
2016-11-08 10:59:50 +0530

    initial commit

---
+++ src/main/java/com/codekul/javabrushup/Main.java
@@ -0,0 +1,12 @@
+package com.codekul.javabrushup;
+
+/**
+ * Created by aniruddha on 8/11/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/com/codekul/javabrushuptest/MainTest.java
@@ -0,0 +1,9 @@
+package com.codekul.javabrushuptest;
+
+/**
+ * Created by aniruddha on 8/11/16.
+ */
+public class MainTest {
+
+
+}
