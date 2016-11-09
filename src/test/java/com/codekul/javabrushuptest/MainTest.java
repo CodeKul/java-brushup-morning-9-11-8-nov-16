b0bce8f5a409e9eef1b31285fa92d9e1605bbd4e
aniruddha <aniruddha.kudalkar@gmail.com>
2016-11-09 10:53:16 +0530

    custom tasks, module , gradle commands

--- src/test/java/com/codekul/javabrushuptest/MainTest.java
+++ src/test/java/com/codekul/javabrushuptest/MainTest.java
@@ -1,9 +1,17 @@
 package com.codekul.javabrushuptest;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by aniruddha on 8/11/16.
  */
 public class MainTest {
 
+    @Test
+    public void checkEqualityOfAge(){
 
+        Assert.assertEquals(12,12);
+        System.out.println("yes its equal");
+    }
 }
---
+++ temp-module/src/main/java/com/codekul/temp/Main.java
@@ -0,0 +1,12 @@
+package com.codekul.temp;
+
+/**
+ * Created by aniruddha on 9/11/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello temp module");
+    }
+}
